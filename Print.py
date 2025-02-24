4636be9005987f104d00de7a91dbb85515a9fd2e
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-24 00:28:53 -0600

    usando print y saltos de linea

---
+++ Print.py
@@ -0,0 +1,12 @@
+#1leccion de print
+print("¡Velocidad! Soy veloz. Un ganador, 42 perdedores")
+print("¡yo desayuno perdedores!")
+
+#2 leccio: print vacio
+print("desayuno?")
+print()
+print("tal vez debi desayunar")
+
+#3 leccion:salto de linea
+print("un desayunito estaria bien\n nonono, concentrate")
+print("velocidad\nsoy mas que rapido!\n mas que veloz\n soy un rayo.")
