f94ce16de2533ffe3382ce2eef17b31db95978e1
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-24 23:47:52 -0600

    Laboratorio de print
    practica de uso de sep y end

---
+++ LAB_argumentos_print.py
@@ -0,0 +1,5 @@
+# Imprime tres palabras separadas por "***" y al final agrega "..." en lugar del salto de línea
+print("Programming", "Essentials", "in", sep="***", end="...")  
+
+# Imprime "Python" en la misma línea porque la primera print() no hizo un salto de línea
+print("Python")  
