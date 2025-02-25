a35bc1faeaa0bf9c54e2befd63f0b8e5eb486808
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-24 23:37:37 -0600

    argumentos clave del print
    usamos el end para hacer una especie de salto de linea y el sep para darle un carácter de espacio entre las palabras

---
+++ Argumentos_plabra_clave.py
@@ -0,0 +1,10 @@
+# Usando 'sep' para cambiar los separadores
+print("Python", "es", "poderoso", sep="°°°°°°°°°")  
+
+# Usando 'end' para evitar el salto de línea y seguir en la misma línea
+print("Esto es el inicio...", end="🚀 ")
+
+
+# Mezclando sep y end
+print("1", "2", "3", "4", sep=" -> ", end=" = Fin de la serie\n")
+
