3c58bbfce089082a10c65286a9c2f12db33ef060
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-25 00:27:03 -0600

    Enteros
    usamos la conversión directa del print para numeros en hexadecimal y octa

---
+++ Enteros_hexa_octa.py
@@ -0,0 +1,9 @@
+print("\nConversión directa en print:")
+print(0o123)  # Octal a decimal
+print("Se espera: 83")
+print(0x123)  # Hexadecimal a decimal
+print("Se espera: 291")
+print(0o777)  # Octal 777 a decimal
+print("Se espera: 511")
+print(0xFFF)  # Hexadecimal FFF a decimal
+print("Se espera: 4095")
