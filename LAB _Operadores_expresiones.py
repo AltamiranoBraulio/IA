adb359c3de0354a13552d1a8c3ce72dc20099271
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-26 10:34:24 -0600

    LAB operadores
    se resolvió una ecuación

---
+++ LAB _Operadores_expresiones.py
@@ -0,0 +1,9 @@
+# Leer el valor de x como un número flotante
+x = float(input("Introduce el valor de x: "))
+
+# Evaluar la expresión 3x^3 - 2x^2 + 3x - 1
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+
+# Imprimir el resultado de y
+print("y =", y)
+
