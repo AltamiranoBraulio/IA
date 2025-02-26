b845d7a8bda61d4167a6f19bb288169ad94765bf
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-26 10:27:36 -0600

    LAB Convertidor
    utilizamos datos sencillo para que pueda hacer una conversión de km a millas

---
+++ LAB_convertidor.py
@@ -0,0 +1,13 @@
+# Datos de entrada
+millas = 7.38
+kilometros = 12.25
+
+# Conversión de millas a kilómetros
+km = millas * 1.61
+
+# Conversión de kilómetros a millas
+mi = kilometros / 1.61
+
+# Imprimir los resultados utilizando la función print() con múltiples argumentos
+print(round(millas, 2), "millas son", round(km, 2), "kilómetros")
+print(round(kilometros, 2), "kilómetros son", round(mi, 2), "millas")
