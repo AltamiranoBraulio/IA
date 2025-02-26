6998b16cfa719e874a9cb50eb3e2a5fc80c5e543
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-26 11:34:47 -0600

    Imput
    Aprendí como utilizar la función imput para ingresar datos

---
+++ Imput.py
@@ -0,0 +1,13 @@
+# Solicitamos al usuario el número de manzanas que tiene
+manzanas = int(input("¿Cuántas manzanas tienes? "))
+
+# Calculamos el número de manzanas al cuadrado
+cuadrado_manzanas = manzanas ** 2
+
+# Calculamos el número de manzanas multiplicado por 3
+triplicado_manzanas = manzanas * 3
+
+# Mostramos los resultados
+print(f"Si tienes {manzanas} manzanas, entonces:")
+print(f"- El cuadrado de tus manzanas es: {cuadrado_manzanas}")
+print(f"- Si multiplicamos tus manzanas por 3, el resultado es: {triplicado_manzanas}")
