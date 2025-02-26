e62d8fe417b44f7ae188e993f27b17a19f1d0801
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-02-26 09:57:10 -0600

    Variables
    se hizo un cuestionario donde se almacenan variables

---
+++ Variables.py
@@ -0,0 +1,16 @@
+# Preguntas y almacenamiento de respuestas en variables
+nombre = input("¿Cuál es tu nombre? ")
+edad = input("¿Cuántos años tienes? ")
+ciudad = input("¿En qué ciudad vives? ")
+comida_favorita = input("¿Cuál es tu comida favorita? ")
+color_favorito = input("¿Cuál es tu color favorito? ")
+hobbie = input("¿Qué te gusta hacer en tu tiempo libre? ")
+
+# Mostrar la información recopilada
+print("\n========== RESPUESTAS ==========")
+print("Tu nombre es:", nombre)
+print("Tienes", edad, "años.")
+print("Vives en", ciudad + ".")
+print("Tu comida favorita es:", comida_favorita)
+print("Tu color favorito es:", color_favorito)
+print("En tu tiempo libre te gusta:", hobbie)
