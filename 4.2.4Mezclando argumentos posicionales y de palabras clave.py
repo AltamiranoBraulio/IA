c66660b7a78cf6f8b5f918953ee9bb632485f289
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 01:38:59 -0600

    Update 4.2.4Mezclando argumentos posicionales y de palabras clave.py

--- 4.2.4Mezclando argumentos posicionales y de palabras clave.py
+++ 4.2.4Mezclando argumentos posicionales y de palabras clave.py
@@ -1,3 +1,13 @@
-def descuento(precio, porcentaje=18):
-    precio_final = precio*(1-porcentaje/100)
-    return precio_final
+def descuento(precio, nombre, porcentaje=18, aplicable=True):
+    if aplicable:
+        precio_descuento = precio*(1-porcentaje/100)
+    else:
+        precio_descuento = precio
+        print(f"El producto {nombre}")
+        print(f"Precio original ${precio}")
+        print(f"Descuento del {porcentaje}%")
+        print(f"Total a pagar ${precio_descuento}")
+        print()
+        print()
+
+        
