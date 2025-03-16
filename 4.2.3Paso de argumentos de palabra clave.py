8305e4a0f8bdc90db97b650f075ba958ab8ccd68
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 01:20:57 -0600

    palabras clave

---
+++ 4.2.3Paso de argumentos de palabra clave.py
@@ -0,0 +1,21 @@
+def datos_empleado (nombre, edad, puesto, salario, oficina):
+    print("Datos de empleado para nuevo ingreso")
+    print(f"Nombre: {nombre}")
+    print(f"Edad: {edad}")      
+    print(f"Puesto: {puesto}")
+    print(f"Salario:${salario}")
+    print(f"Ubicacion: {oficina}")
+    print("¡Bienvenido a la empresa!")
+    print()
+    print()
+
+
+datos_empleado("Pedro", 18, "Gerente", 20000, "Oficina 1")
+datos_empleado("Juan", 20, "Contador", 15000, "Oficina 2")  
+datos_empleado ("Maria", 22, "Secretaria", 10000,"Oficina 3")
+datos_empleado("Jose", 24, "Ingeniero", 25000, "Oficina 4")
+datos_empleado("Ana", 26, "Recepcionista", 8000, "Oficina 5")
+datos_empleado("Luis", 28, "Vigilante", 5000, "Caseta")
+datos_empleado("Laura", 30, "Mensajero", 7000, "No asignada")
+datos_empleado("Carlos", 32, "Chofer", 9000, "No asignada")
+    
