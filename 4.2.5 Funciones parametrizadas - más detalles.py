71d25c174ed2a758fc4f2325e45cb912e24d4019
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 17:01:19 -0600

    Update 4.2.5 Funciones parametrizadas - más detalles.py

--- 4.2.5 Funciones parametrizadas - más detalles.py
+++ 4.2.5 Funciones parametrizadas - más detalles.py
@@ -1,1 +1,9 @@
-def cafe(tipo="americano", tamano="mediano", ):
+def cafe(tipo="americano", tamano="mediano", azucar=1, leche=false, extra=None ):
+    descricion="preparando café {tipo} {tamano}"
+    if leche:
+        descricion+=" con leche"
+    if extra:
+        descricion+=" y "+extra
+    descricion + f", con {azucar} cucharadas de azúcar"
+
+    
