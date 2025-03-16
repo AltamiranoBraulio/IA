3b135ecf42be54c2eda575636188b7035844a297
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 00:58:02 -0600

    Update 4.2.2Paso de parámetros posicionales.py

--- 4.2.2Paso de parámetros posicionales.py
+++ 4.2.2Paso de parámetros posicionales.py
@@ -2,7 +2,7 @@ def datos(nombre, edad, ciudad):
     print(f"Hola mi nombre es {nombre}", f"tengo {edad} años", f"y vivo en {ciudad}")
    
 
-datos("pedro", 18, "Jerusalem")
-datos("juan", 20, "Veracruz")
-datos("maria", 22, "Tepatitlan")
-datos("jose", 24, "San Carlos")
+datos("Pedro", 18, "Jerusalem")
+datos("Juan", 20, "Veracruz")
+datos("Maria", 22, "Tepatitlan")
+datos("Jose", 24, "San Carlos")
