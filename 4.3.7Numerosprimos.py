402a9567ba9d74f4a4460a603a5b53f04f991b19
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 20:23:24 -0600

    Update 4.3.7Numerosprimos.py

--- 4.3.7Numerosprimos.py
+++ 4.3.7Numerosprimos.py
@@ -7,12 +7,12 @@ def is_prime(num):
         return False
     
 
-for i in range(3, int(num**0.5 )+ 1, 2):
+for i in range(3, int(num **0.5 )+ 1, 2):
     if num % i == 0:
         return False
     return True
 
-forn i in range(1, 20):
+for i in range(1, 20):
      if is_prime(i):
     print(i, end=" ")   
 print()
