4cdd12712eaf7df18d26918f848cce02cc533aea
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 22:35:34 -0600

    Update 4.6.3 Diccionarios.py

--- 4.6.3 Diccionarios.py
+++ 4.6.3 Diccionarios.py
@@ -2,3 +2,7 @@ agenda = {}
 def agregar_contacto(nombre, telefono):
     agenda[nombre] = telefono
     print(f"Contacto '{nombre}' agregado con éxito.")
+    if nombre in agenda:
+        print(f"Nombre: {nombre}, Teléfono: {agenda[nombre]}")
+    else:
+        print(f"El contacto '{nombre}' no existe en la agenda.")
