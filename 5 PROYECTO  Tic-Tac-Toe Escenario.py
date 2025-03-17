2d243607f5a563f6b133ba178b1840ae8a43d84c
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 23:13:13 -0600

    Update 5 PROYECTO  Tic-Tac-Toe Escenario.py

--- 5 PROYECTO  Tic-Tac-Toe Escenario.py
+++ 5 PROYECTO  Tic-Tac-Toe Escenario.py
@@ -6,3 +6,12 @@ def mostrar_tablero(tablero):
         print(f"|   {fila[0]}   |   {fila[1]}   |   {fila[2]}   |")
         print("|       |       |       |")
         print("+-------+-------+-------+")
+def verificar_ganador(tablero, jugador):
+    for i in range(3):
+        if all([celda == jugador for celda in tablero[i]]):  # Filas
+            return True
+        if all([tablero[j][i] == jugador for j in range(3)]):  # Columnas
+            return True
+    if all([tablero[i][i] == jugador for i in range(3)]):  # Diagonal principal
+        return True
+    
