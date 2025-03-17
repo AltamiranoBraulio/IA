02a4eaf46962476e1c4b55b9ebde15fc4fedf54c
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 18:33:08 -0600

    none

---
+++ 4.3.2None.py
@@ -0,0 +1,18 @@
+def chicles (chicles_disponibles):
+    if chicles_disponibles > 0:
+        return "chicles"
+    else:
+        return "Producto no disponible"
+    
+
+comprar = chicles(3)
+    if compra is not None:
+    print(f"obtuviste un paquete de {comprar}")
+    else:   
+        print("Producto no disponible")
+
+comprar = chicles(0)
+    if compra is not None:
+    print(f"obtuviste un paquete de {comprar}")
+    else:   
+        print("Producto no disponible")
