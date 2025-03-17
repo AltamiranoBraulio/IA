d6aefa4031dc72bc5fc646e386845cafd0327807
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 22:36:10 -0600

    Update 4.6.3 Diccionarios.py

--- 4.6.3 Diccionarios.py
+++ 4.6.3 Diccionarios.py
@@ -6,3 +6,9 @@ def agregar_contacto(nombre, telefono):
         print(f"Nombre: {nombre}, Teléfono: {agenda[nombre]}")
     else:
         print(f"El contacto '{nombre}' no existe en la agenda.")
+def mostrar_agenda():
+    if agenda:
+        print("\n--- Agenda de contactos ---")
+        for nombre, telefono in agenda.items():
+            print(f"Nombre: {nombre}, Teléfono: {telefono}")
+    else:
