af5cc19d365b862f7dffbb35555d92d69f61b1d3
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 21:11:43 -0600

    interacciones

---
+++ 4.4.3Interacion función con argumentos.py
@@ -0,0 +1,20 @@
+def agregar_fruta(inventario, fruta):
+    inventario.append(fruta)
+    print(f"Fruta agregada: {fruta}")
+
+
+def manzanas(manazanas_disponibles):
+   manzanas_disponibles= 5
+   print(f"se modificaron las manzanas ahora son {manzanas_disponibles}")
+
+
+
+inventario_frutas = ["manzanas", "peras", "uvas"]
+
+manzanas_en_inventario =10
+
+agregar_fruta(inventario_frutas, "naranjas")
+modificar_manzanas(manzanas_en_inventario)
+
+print("|nventario de frutas:", inventario_frutas)
+print(f"manzanas en inventario: {manzanas_en_inventario}")
