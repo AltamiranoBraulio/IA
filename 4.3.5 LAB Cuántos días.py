2b73ec04dcc96f1b10e9b682e8817d2ecba45b9b
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 19:54:38 -0600

    Update 4.3.5 LAB Cuántos días.py

--- 4.3.5 LAB Cuántos días.py
+++ 4.3.5 LAB Cuántos días.py
@@ -9,4 +9,15 @@ def is_year_leap(year):
         return False
     
 
-    def day 
+def days_in_month(year, month):
+    if month < 1 or month > 12 or year < 1
+       return None
+    
+    day_per_month= [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+
+    if month == 2 and is_year_leap(year):
+        return 29
+    return day_per_month[month]
+
+
+test
