cbb5014e8edce1cbf12b7925756ab7d51f7e332d
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 21:45:06 -0600

    Update 4.6.2 Tuplas.py

--- 4.6.2 Tuplas.py
+++ 4.6.2 Tuplas.py
@@ -5,11 +5,11 @@ def calcular_distancia(coordenadas):
     distancia = math.sqrt(x**2 + y**2)
     return distancia
 
-punto (8,3)
+punto =(8,3)
 
 
 print(f"las coordenadas son: {punto}")
 
 distancia_origen= calcular_distancia(punto)
-print(f"la distancia al origen es: {distancia_origen}unidades")
+print(f"la distancia al origen es: {distancia_origen} unidades")
   
