8aea814d028e33e6f9b726c11e27e41abc472734
AltamiranoBraulio <143303555+AltamiranoBraulio@users.noreply.github.com>
2025-03-16 19:43:23 -0600

    Update 4.3.4LAB año bisiesto.py

--- 4.3.4LAB año bisiesto.py
+++ 4.3.4LAB año bisiesto.py
@@ -8,3 +8,13 @@ def is_year_leap(year):
     else:
         return False
 
+test data = [1900, 2000, 2016, 1987]
+test results = [False, True, True, False]
+
+for i in range(Len(test_data)):
+    yr = test_data[i]
+    print(yr,"->",end="")
+    if is_year_leap(yr):
+        print("ok")
+    else:
+        print("fallido")
